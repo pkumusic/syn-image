a2cd2b56d56c7941a20baa9170685f215b28a317
lyzmusic <635716260@qq.com>
2017-03-05 20:28:28 -0500

    - add conv3 and conv4

--- image_feature.py
+++ image_feature.py
@@ -11,7 +11,8 @@ import argparse
 #import torchvision.datasets.coco
 # How to extract features of an image from a trained model : https://discuss.pytorch.org/t/how-to-extract-features-of-an-image-from-a-trained-model/119/2
 # TODO: implement a dataset class for flowers
-def main(args):
+import glob
+def flowers(args):
     data_folder = 'data/102flowers/'
     labels = load_labels(data_folder + 'imagelabels.txt')
     #  labels = load_labels(data_folder + 'imagelabels_10class.txt')
@@ -19,6 +20,8 @@ def main(args):
     print 'total number: ', n
     # Model initialization
     features = []
+    conv3s = []
+    conv4s = []
     model = resnet152(pretrained=True)
     # alexnet = models.alexnet()
     # vgg16 = models.vgg16()
@@ -51,14 +54,86 @@ def main(args):
         image = Variable(image)
         if args.cuda:
             feature = model.forward(image, until_layer='fc').data.cpu().numpy()
+            conv3 = model.forward(image, until_layer='3').data.cpu.numpy()
+            conv3 = conv3.reshape(1,-1)
+            conv4 = model.forward(image, until_layer='4').data.cpu.numpy()
+            conv4 = conv4.reshape(1,-1)
         else:
             feature = model.forward(image, until_layer='fc').data.numpy()
+            conv3 = model.forward(image, until_layer='3').data.numpy()
+            conv3 = conv3.reshape(1,-1)
+            conv4 = model.forward(image, until_layer='4').data.numpy()
+            conv4 = conv4.reshape(1,-1)
         #print feature
         #print feature.shape
         #exit()
         features.append(feature)
+        conv3s.append(conv3)
+        conv4s.append(conv4)
     features = np.concatenate(features, axis=0)
     np.save('fc_features_res152', features)
+    np.save('fc_conv3_res152', conv3s)
+    np.save('fc_conv4_res152', conv4s)
+
+# def birds(args):
+#     data_folder = 'data/CUB_200_2011/CUB_200_2011'
+#     folders = glob.glob(data_folder + '/images/*')
+#     label = 0
+#     image_label_pairs = []
+#     for folder in folders:
+#         label += 1
+#         image_paths = glob.glob(folder + '/*')
+#         for image_path in image_paths:
+#             image = load_image(image_path)
+#             image_label_pairs.append((label, image))
+#     print image_label_pairs
+#
+#     labels = load_labels(data_folder + 'imagelabels.txt')
+#     #  labels = load_labels(data_folder + 'imagelabels_10class.txt')
+#     n = len(labels)
+#     print 'total number: ', n
+#     # Model initialization
+#     features = []
+#     model = resnet152(pretrained=True)
+#     # alexnet = models.alexnet()
+#     # vgg16 = models.vgg16()
+#     if torch.cuda.is_available():
+#         if not args.cuda:
+#             print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+#         else:
+#             model = model.cuda()
+#     for i in xrange(n):
+#         #if i % 100 == 0:
+#         print i
+#         # Load image from file
+#         image = load_image(data_folder + 'jpg/image_%.5d.jpg'%(i+1))
+#         label = labels[i]
+#
+#         # Perform transform
+#         image = image.resize((224,224))
+#         transform = transforms.Compose([
+#             # transforms.Scale((224)),
+#             transforms.ToTensor(),
+#         ])
+#         # transform = transforms.ToTensor()
+#         image = transform(image)
+#         #  save_image(image, 'test.jpg')
+#
+#         image.resize_((1,) + image.size())
+#
+#         if args.cuda:
+#             image = image.cuda()
+#         image = Variable(image)
+#         if args.cuda:
+#             feature = model.forward(image, until_layer='fc').data.cpu().numpy()
+#         else:
+#             feature = model.forward(image, until_layer='fc').data.numpy()
+#         #print feature
+#         #print feature.shape
+#         #exit()
+#         features.append(feature)
+#     features = np.concatenate(features, axis=0)
+#     np.save('fc_features_res152', features)
 
 def extract(args):
 
@@ -114,7 +189,8 @@ if __name__ == "__main__":
     parser.add_argument('--cuda', action='store_true', help='use CUDA')
     args = parser.parse_args()
     #  extract(args)
-    main(args)
+    flowers(args)
+    #birds(args)
     #visualize_filter()
 
 
